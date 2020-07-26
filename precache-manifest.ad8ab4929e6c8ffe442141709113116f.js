2219114df3283c7db47b8944318aa9a0939085bd
Josh Gearheart <other.josh.gearheart@gmail.com>
2020-07-26 12:38:27 -0700

    Updates

---
+++ precache-manifest.ad8ab4929e6c8ffe442141709113116f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a86ae6dc365e17f3c94cd1c07392d1",
+    "url": "/cardavan/index.html"
+  },
+  {
+    "revision": "075d9ee47ebf1d17f5b2",
+    "url": "/cardavan/static/css/main.ad779244.chunk.css"
+  },
+  {
+    "revision": "0ed4d8a8941e940769a5",
+    "url": "/cardavan/static/js/2.35a30a4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cardavan/static/js/2.35a30a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075d9ee47ebf1d17f5b2",
+    "url": "/cardavan/static/js/main.aeefdb67.chunk.js"
+  },
+  {
+    "revision": "6d429e21ee0208dda157",
+    "url": "/cardavan/static/js/runtime-main.2c7ecad7.js"
+  }
+]);
